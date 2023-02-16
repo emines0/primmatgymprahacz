73bc45d92fe0edbb75d663a8d10720f27e62c543
emines0 <emin.sefa1@gmail.com>
2023-02-16 13:56:35 +0100

    adding gtag scirpt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Navigation from './components/Navigation/Navigation'
 import { navLinks } from './assets/data/navigation'
-
+import ReactGA from 'react-ga4'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { useEffect } from 'react'
+
+const trackingId = 'UA-132431767-1'
+ReactGA.initialize(trackingId)
 
 function App() {
   return (
