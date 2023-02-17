42133192f6195f492cb3081e3d572f5bfba84833
emines0 <emin.sefa1@gmail.com>
2023-02-17 11:45:16 +0100

    adjusting heading in the About sectioh

--- src/components/_pages/About/About.js
+++ src/components/_pages/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <main>
       <section className='container about'>
-        <h1 className='page-heading'>Napsali O Nás</h1>
+        <h1 className='page-heading'>Napsali o nás</h1>
         <div className='container-about'>
           {about
             .sort((a, b) => b.id - a.id)
